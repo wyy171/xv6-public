727a9aac728dfea15b9ae427aa8fc6b7d6255ae5
wyy171 <94099374+wyy171@users.noreply.github.com>
2023-09-09 10:37:15 -0400

    Update head.c

--- head.c
+++ head.c
@@ -11,7 +11,7 @@ void print_head_lines(int input_fd, int n){
         if (m <= 0) {
             break; // End of file or error
         }
-    printf(2, "%s\n", current_line);
+        printf(2, "%s\n", current_line);
         count++;
     }
 }
