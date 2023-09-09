ed1a5e18059186ae1c2a8a7e500c01042a059d37
wyy171 <94099374+wyy171@users.noreply.github.com>
2023-09-09 10:59:52 -0400

    Update head.c

--- head.c
+++ head.c
@@ -20,7 +20,7 @@ void print_head_lines(int input_fd, int n){
 int main(int argc, char *argv[]) {
 
     int lines = 4;
-
+     printf(2, "%d, %s, %s", argc, argv[1],argv[2]);
     // Parse command-line arguments
     if(argc == 3)
         lines = atoi(argv[2]);
