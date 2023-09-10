c4f6922b1d45cdb2f1b62f528751339ece936836
wyy171 <94099374+wyy171@users.noreply.github.com>
2023-09-10 11:47:46 -0400

    Update head.c

--- head.c
+++ head.c
@@ -32,7 +32,7 @@ head_n(int input_fd, int lines) {
 
 int main(int argc, char *argv[]) {
     int fd;
-    int lines = 4;
+    int lines = 10;
     
     if(argc < 2){
         head_n(0, lines);
@@ -47,9 +47,13 @@ int main(int argc, char *argv[]) {
         exit();
     }
     if(argc > 2){
-        lines = atoi(argv[2]);  
-        if((fd = open(argv[1],0)) < 0){
-            printf(2, "head: cannot open %s \n", argv[1]);
+        lines = atoi(argv[1]);  
+        if((fd = open(argv[2],0)) < 0){
+            printf(2, "head: cannot open %s \n", argv[2]);
+            exit();
+          }
+        if((argv[0] != "-n"){
+            printf(2, "head: need -n not %s", argv[0]);
             exit();
           }
          head_n(fd, lines);
