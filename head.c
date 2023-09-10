0e8ba6897ad2af6b241dc2dd0fae04b1c49b9bac
wyy171 <94099374+wyy171@users.noreply.github.com>
2023-09-10 00:29:31 -0400

    Update head.c

--- head.c
+++ head.c
@@ -1,7 +1,8 @@
-#include "types.h"
+#include "defs.h"
+#include "file.h"
+#include "fcntl.h"
+#include "param.h"
 #include "stat.h"
-#include "user.h"
-
 
 void print_head_lines(int input_fd, int n){
     int count = 0;
@@ -17,20 +18,40 @@ void print_head_lines(int input_fd, int n){
         count++;
     }
 }
+void khead(int fd, int n) {
+    char buf[512];
+    int count = 0;
 
+    while (count < n) {
+        int nread = read(fd, buf, sizeof(buf));
+        if (nread <= 0)
+            break;
+
+        for (int i = 0; i < nread; i++) {
+            char c = buf[i];
+            putchar(c); // Replace with your kernel-specific output function
+            if (c == '\n')
+                count++;
+        }
+    }
+}
 
 int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        cprintf("Usage: myhead <file> <n>\n");
+        exit();
+    }
 
-    int lines = 4;
-     printf(2, "%d, %s, %s", argc, argv[1],argv[2]);
-    // Parse command-line arguments
-    if(argc == 3)
-        lines = atoi(argv[2]);
-    
-    printf(2, "Uniq command is getting executed in kernel mode.\n");
-    
-    // Invoke the uniq system call
-    print_head_lines(0,lines);
+    int fd = open(argv[1], O_RDONLY);
+    if (fd < 0) {
+        cprintf("myhead: cannot open %s\n", argv[1]);
+        exit();
+    }
+
+    int n = atoi(argv[2]);
+    khead(fd, n);
+
+    close(fd);
 
     exit();
 }
