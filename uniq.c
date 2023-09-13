d5e6b5858e07cc9a96cbb1f9d075bc37411e562e
wyy171 <94099374+wyy171@users.noreply.github.com>
2023-09-12 23:22:54 -0400

    Update uniq.c

--- uniq.c
+++ uniq.c
@@ -49,15 +49,16 @@ uniq_compare(int input_fd, int output_fd, int cflag, int iflag, int dflag) {
         }
 
         // Implement -d flag logic ( It only prints the repeated lines and not the lines which aren’t repeated.)
-        if (!dflag || strcmp(current_line, prev_line) != 0) {
-            if (count > 1) {
+        if (strcmp(current_line, prev_line) != 0 && count != 0) {
+            if (cflag) {
                 // Output the count and line if -c flag is set
                 printf(output_fd, "%d %s\n", count, prev_line);
-            } else if (count == 0 && !dflag) {
+            } else if (dflag && count > 1) {
                 // Output the unique line (if not using -d)
                 printf(output_fd, "%s\n", prev_line);
+            } else if (!dflag) {
+                printf(output_fd, "%s\n", prev_line);
             }
-            
             // Reset the count for the new line
             count = 1;
         } else {
