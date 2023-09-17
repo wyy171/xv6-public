19e210b59651247e4fe3df8573a2172dd25390d3
wyy171 <94099374+wyy171@users.noreply.github.com>
2023-09-17 00:07:21 -0400

    Create test.c

---
+++ test.c
@@ -0,0 +1,24 @@
+#include "types.h"
+#include "user.h"
+
+int main() {
+    struct proc_stat pstat;
+
+    int pid = fork();
+    if (pid == 0) {
+        exec("uniq", argv);
+    } else {
+        wait(&pstat);
+        printf(1, "Process uniq: ctime=%d, etime=%d, rtime=%d\n", pstat.ctime, pstat.etime, pstat.rtime);
+    }
+
+    pid = fork();
+    if (pid == 0) {
+        exec("head", argv);
+    } else {
+        wait(&pstat);
+        printf(1, "Process head: ctime=%d, etime=%d, rtime=%d\n", pstat.ctime, pstat.etime, pstat.rtime);
+    }
+
+    exit();
+}
