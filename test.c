2be7b8df12bfadfbb5eb1f9e1fe2511606d86c0b
wyy171 <94099374+wyy171@users.noreply.github.com>
2023-09-19 12:19:29 -0400

    Update test.c

--- test.c
+++ test.c
@@ -23,3 +23,31 @@ int main() {
 
     exit();
 }
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(int argc, char *argv[])
+{
+  if(argc < 2) {
+    printf(2, "Usage: waitx command\n");
+    exit();
+  }
+
+  int pid = fork();
+  if(pid == -1) printf(2, "Fork Error\n");
+  else if(pid == 0)
+  {
+    if(exec(argv[1], argv + 1) == -1) printf(2, "No such command\n");
+    exit();
+  }
+  else
+  {
+    int rtime, wtime;
+    waitx(&rtime, &wtime);
+    printf(1, "Running Time : %d\nWaiting Time : %d\n", rtime, wtime);
+  }
+
+  exit();
+}
