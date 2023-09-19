e660fffcee2df117641e409ba00dcb75f585e9ea
wyy171 <94099374+wyy171@users.noreply.github.com>
2023-09-19 13:04:27 -0400

    Update test.c

--- test.c
+++ test.c
@@ -1,15 +1,15 @@
 #include "types.h"
+#include "stat.h"
 #include "user.h"
 
 int main() {
-    #struct proc_stat pstat;
+    struct proc pstat;
 
     int pid = fork();
     if (pid == 0) {
         exec("uniq", argv);
     } else {
-        #wait(&pstat);
-        wait();
+        wait(&pstat);
         printf(1, "Process uniq: ctime=%d, etime=%d, rtime=%d\n", pstat.ctime, pstat.etime, pstat.rtime);
     }
 
@@ -22,32 +22,4 @@ int main() {
     }
 
     exit();
-}
-#include "types.h"
-#include "stat.h"
-#include "user.h"
-
-int
-main(int argc, char *argv[])
-{
-  if(argc < 2) {
-    printf(2, "Usage: waitx command\n");
-    exit();
-  }
-
-  int pid = fork();
-  if(pid == -1) printf(2, "Fork Error\n");
-  else if(pid == 0)
-  {
-    if(exec(argv[1], argv + 1) == -1) printf(2, "No such command\n");
-    exit();
-  }
-  else
-  {
-    int rtime, wtime;
-    waitx(&rtime, &wtime);
-    printf(1, "Running Time : %d\nWaiting Time : %d\n", rtime, wtime);
-  }
-
-  exit();
 }
