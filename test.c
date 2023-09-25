a4ceb94ce52ef5f0ad0f9f39be3c3ef7068b214f
wyy171 <94099374+wyy171@users.noreply.github.com>
2023-09-24 23:43:03 -0400

    Update test.c

--- test.c
+++ test.c
@@ -2,7 +2,32 @@
 #include "stat.h"
 #include "user.h"
 
-int main(int argc, char *argv[]){
+int
+main(int argc, char *argv[])
+{
+  if(argc < 2) {
+    printf(2, "Usage: test command\n");
+    exit();
+  }
+
+  int pid = fork();
+  if(pid == -1) printf(2, "Fork Error\n");
+  else if(pid == 0)
+  {
+    if(exec(argv[1], argv + 1) == -1) printf(2, "No such command\n");
+    exit();
+  }
+  else
+  {
+    int ctime, etime;
+    waitx(&ctime, &etime);   
+    printf(1, "Process %s: ctime=%d, etime=%d, rtime=%d\n", argv[1], ctime, etime, etime-ctime);
+  }
+
+  exit();
+}
+
+/*int main(int argc, char *argv[]){
 
     int pid = fork();
     if (pid == 0) {
@@ -26,6 +51,6 @@ int main(int argc, char *argv[]){
     }
 
     exit();
-}
+}*/
 
 
