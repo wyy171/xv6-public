022a72772dcaaa63f992dc4e7fac19f50a910d11
wyy171 <94099374+wyy171@users.noreply.github.com>
2023-10-24 22:34:01 -0400

    Create dpro.c

---
+++ dpro.c
@@ -0,0 +1,42 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+int main(int argc, char *argv[])
+{
+    int k, n, pid;
+    int x=0, z;
+
+    int ipr;
+
+    if(argc < 2)
+        n = 1;  // default value
+    else
+        n = atoi(argv[1]);  // from user input
+    if (n<0 || n>20)
+        n = 2;
+    x=0;
+    pid = 0;
+    
+    for(k = 0; k<0; k++){
+        pid = fork();
+        if(pid < 0)
+            printf(1, "%d failed in fork!\n", getpid());
+        else if(pid > 0)
+        {   // Parent
+            int id1 = getpid();
+            chpr(id1, ipr);
+            printf(1, "Parent %d creating child %d\n", getpid(), pid);
+            wait();
+        }
+        else
+        {   // Child
+            printf(1, "Child %d created\n", getpid());
+            for(z=0;z<4000000000;z+=1)
+                x = x + 3.14*89.64; // Useless calculations to consume CPU time
+            break;
+        }
+    }
+    exit();
+}
