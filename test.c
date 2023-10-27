0e9a981a00f61ad0b52fb68a7cb63a2768aee494
wyy171 <94099374+wyy171@users.noreply.github.com>
2023-10-27 00:27:55 -0400

    Update test.c

--- test.c
+++ test.c
@@ -8,7 +8,7 @@ void long_task(int priority, char* program) {
         printf(1, "Fork failed.\n");
     } else if (pid == 0) {
         // Child process
-        set_priority(priority); // Set the priority
+        setpr(priority); // Set the priority
         exec(program, 0);
         printf(1, "Exec failed for %s\n", program);
         exit();
