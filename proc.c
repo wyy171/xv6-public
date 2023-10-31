f31745f81e4155bcd96ca3137da7c1a825f0008a
wyy171 <94099374+wyy171@users.noreply.github.com>
2023-10-31 09:00:52 -0400

    Update proc.c

--- proc.c
+++ proc.c
@@ -449,6 +449,52 @@ setpr(int pid, int priority)
 void
 scheduler(void)
 {
+  struct proc *p, *p1;
+  struct cpu *c = mycpu();
+  c->proc = 0;
+  
+  for(;;){
+    // Enable interrupts on this processor.
+    sti();
+    struct proc *highP;
+    // Loop over process table looking for process to run.
+    acquire(&ptable.lock);
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->state != RUNNABLE)
+        continue;
+
+      // Switch to chosen process.  It is the process's job
+      // to release ptable.lock and then reacquire it
+      // before jumping back to us.
+      highP = p;
+      //choose one with highest priority
+      for(p1 = ptable.proc; p1 < &ptable.proc[NPROC]; p1++){
+	if(p1->state != RUNNABLE)
+	  continue;
+	if(highP->priority > p1->priority)   //larger value, lower priority
+	  highP = p1;
+      }
+      p = highP;
+      c->proc = p;
+      switchuvm(p);
+      p->state = RUNNING;
+
+      swtch(&(c->scheduler), p->context);
+      switchkvm();
+
+      // Process is done running for now.
+      // It should have changed its p->state before coming back.
+      c->proc = 0;
+    }
+    release(&ptable.lock);
+
+  }
+}
+
+/*
+void
+scheduler(void) //works
+{
   struct proc *p;
   struct proc *p1;
   struct cpu *c = mycpu();
@@ -491,7 +537,7 @@ scheduler(void)
 
   }
 }
-
+*/
 /* Kai
 void
 scheduler(void)
