239f11191c265324d6a8c8cb8de9a2d93dae34d5
wyy171 <94099374+wyy171@users.noreply.github.com>
2023-11-08 08:48:13 -0500

    Update exec.c

--- exec.c
+++ exec.c
@@ -39,7 +39,7 @@ exec(char *path, char **argv)
     goto bad;
 
   // Load program into memory.
-  sz = 0;
+  sz=PGSIZE; //sz = 0; changed in project4
   for(i=0, off=elf.phoff; i<elf.phnum; i++, off+=sizeof(ph)){
     if(readi(ip, (char*)&ph, off, sizeof(ph)) != sizeof(ph))
       goto bad;
