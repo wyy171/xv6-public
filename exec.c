ef2537b4e46367223ec3243bf41f6a650e430e6c
wyy171 <94099374+wyy171@users.noreply.github.com>
2023-11-20 21:50:36 -0500

    Update exec.c

--- exec.c
+++ exec.c
@@ -1,3 +1,5 @@
+// In exec.c
+
 #include "types.h"
 #include "param.h"
 #include "memlayout.h"
@@ -10,100 +12,118 @@
 int
 exec(char *path, char **argv)
 {
-  char *s, *last;
-  int i, off;
-  uint argc, sz, sp, hp, cp, ustack[3+MAXARG+1];
-  struct elfhdr elf;
   struct inode *ip;
   struct proghdr ph;
   pde_t *pgdir, *oldpgdir;
-  struct proc *curproc = myproc();//TODO
+  uint sz, sp, up, hp, cp;
+  int i, argc;
+  char *s, *last;
 
+  // Open the file
   begin_op();
-
-  if((ip = namei(path)) == 0){
+  if ((ip = namei(path)) == 0) {
     end_op();
     cprintf("exec: fail\n");
     return -1;
   }
   ilock(ip);
-  pgdir = 0;
 
   // Check ELF header
-  if(readi(ip, (char*)&elf, 0, sizeof(elf)) != sizeof(elf))
+  if (readi(ip, (char*)&ph, 0, sizeof(ph)) != sizeof(ph)) {
     goto bad;
-  if(elf.magic != ELF_MAGIC)
+  }
+  if (ph.magic != ELF_MAGIC) {
     goto bad;
+  }
 
-  if((pgdir = setupkvm()) == 0)
+  // Setup kernel page table
+  pgdir = 0;
+  if ((pgdir = setupkvm()) == 0) {
     goto bad;
+  }
 
-  // Load program into memory.
+  // Load program into memory
   sz = 0;
-  for(i=0, off=elf.phoff; i<elf.phnum; i++, off+=sizeof(ph)){
-    if(readi(ip, (char*)&ph, off, sizeof(ph)) != sizeof(ph))
+  for (i = 0; i < ph.phnum; i++) {
+    if (readi(ip, (char*)&ph, ph.phoff + i * sizeof(ph), sizeof(ph)) != sizeof(ph)) {
       goto bad;
-    if(ph.type != ELF_PROG_LOAD)
+    }
+    if (ph.type != ELF_PROG_LOAD) {
       continue;
-    if(ph.memsz < ph.filesz)
+    }
+    if (ph.memsz < ph.filesz) {
       goto bad;
-    if(ph.vaddr + ph.memsz < ph.vaddr)
+    }
+    if (ph.vaddr + ph.memsz < ph.vaddr) {
       goto bad;
-    if((sz = allocuvm(pgdir, sz, ph.vaddr + ph.memsz)) == 0)
+    }
+    if ((sz = allocuvm(pgdir, sz, ph.vaddr + ph.memsz)) == 0) {
       goto bad;
-    if(ph.vaddr % PGSIZE != 0)
+    }
+    if (ph.vaddr % PGSIZE != 0) {
       goto bad;
-    if(loaduvm(pgdir, (char*)ph.vaddr, ip, ph.off, ph.filesz) < 0)
+    }
+    if (loaduvm(pgdir, (char*)ph.vaddr, ip, ph.off, ph.filesz) < 0) {
       goto bad;
+    }
   }
   iunlockput(ip);
   end_op();
   ip = 0;
 
-  sp = 0;
+  // Allocate two pages at the next page boundary.
+  // Make the first inaccessible. Use the second as the user stack.
+  sz = PGROUNDUP(sz);
+  if ((sz = allocuvm(pgdir, sz, sz + 2*PGSIZE)) == 0) {
+    goto bad;
+  }
+  clearpteu(pgdir, (char*)(sz - 2*PGSIZE));
+  sp = sz;
 
-  sz = PGROUNDUP(sz); // round up user code to be a full page
-
-  
   // Set the starting address of the stack, heap, and code
-  sp = USERTOP;          // Assuming USERTOP is 640KB
+  up = 0;
   hp = sp - PGSIZE * 5;  // Leave at least 5 pages unallocated between stack and heap
   cp = hp - PGSIZE;       // Code starts right before the heap
 
   // Allocate a page for the stack
-  if (allocuvm(pgdir, sp - PGSIZE, sp) == 0)
+  if (allocuvm(pgdir, sp - PGSIZE, sp) == 0) {
     goto bad;
+  }
 
   // Allocate a gap of at least 5 pages
   for (char *va = (char *)(sp - PGSIZE); va > (char *)hp; va -= PGSIZE) {
-    if (allocuvm(pgdir, (uint)va - PGSIZE, (uint)va) == 0)
+    if (allocuvm(pgdir, (uint)va - PGSIZE, (uint)va) == 0) {
       goto bad;
+    }
   }
 
   // Allocate a page for the heap
-  if (allocuvm(pgdir, hp - PGSIZE, hp) == 0)
+  if (allocuvm(pgdir, hp - PGSIZE, hp) == 0) {
     goto bad;
+  }
 
   // Allocate a page for the code
-  if (allocuvm(pgdir, cp - PGSIZE, cp) == 0)
+  if (allocuvm(pgdir, cp - PGSIZE, cp) == 0) {
     goto bad;
-/*
+  }
+
   // Set the program's data pages to be invalid
-  for (char *va = (char *)(cp - PGSIZE); va > (char *)0; va -= PGSIZE) {
-    // Use walkaddr to get the page table entry
-    pte_t *pte = walkaddr(pgdir, va, 0);
-    if (pte == 0)
+  for (char *va = (char *)(cp - PGSIZE); va > (char *)up; va -= PGSIZE) {
+    // Use walkpgdir to get the page table entry
+    pte_t *pte = walkpgdir(pgdir, va, 0);
+    if (pte == 0) {
       goto bad;
+    }
     *pte &= ~PTE_P; // Mark the page table entry as invalid (clear the PTE_P bit)
   }
-  
-  */
+
   // Push argument strings, prepare rest of stack in ustack.
-  for(argc = 0; argv[argc]; argc++) {
-    if(argc >= MAXARG)
+  for (argc = 0; argv[argc]; argc++) {
+    if (argc >= MAXARG) {
       goto bad;
+    }
     sp = (sp - (strlen(argv[argc]) + 1)) & ~3;
-    if(copyout(pgdir, sp, argv[argc], strlen(argv[argc]) + 1) < 0)
+    if (copyout(pgdir, sp, argv[argc], strlen(argv[argc]) + 1) < 0)
       goto bad;
     ustack[3+argc] = sp;
   }
