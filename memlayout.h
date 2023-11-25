a9431c5f6c43c6a8b06964a31c06775acde5e335
wyy171 <94099374+wyy171@users.noreply.github.com>
2023-11-24 23:20:07 -0500

    Update memlayout.h

--- memlayout.h
+++ memlayout.h
@@ -1,5 +1,4 @@
 // Memory layout
-#define USERTOP 0xDD4E000
 #define EXTMEM  0x100000            // Start of extended memory
 #define PHYSTOP 0xE000000           // Top physical memory
 #define DEVSPACE 0xFE000000         // Other devices are at high addresses
